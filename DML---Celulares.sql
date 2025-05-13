842420710908d52f7cd1615dfb5d4fa51ea817ac
YordyHD <alexanderyordy5@gmail.com>
2025-05-13 17:27:18 -0500

    v2

--- DML---Celulares.sql
+++ DML---Celulares.sql
@@ -1,4 +1,24 @@
-CALL datosf
-	(" ","Hewlett-Packard")
-	("","Samsung")
+CALL datosf("Asus");
+CALL datosf("Lenovo");
+CALL datosf("Hewlett-Packard");
+CALL datosf("Samsung");
+CALL datosf("Seagate");
+CALL datosf("Crucial");
+CALL datosf("Gigabyte");
+CALL datosf("Huawei");
+CALL datosf("Xiaomiii");
+
+call datosp("Disco duro SATA3 1TB", 86.99, 5);
+call datosp("Memoria RAM DDR4 8GB ", 120, 6);
+call datosp("Disco SSD 1 TB ", 150.99, 4);
+call datosp("GeForce GTX 1050Ti ", 185, 7);
+call datosp("GeForce GTX 1080 Xtreme ", 755, 6);
+call datosp("Monitor 24 LED Full HD B", 202, 1);
+call datosp("Monitor 27 LED Full HD", 245.99, 1);
+call datosp("Portátil Yoga 520", 559, 2);
+call datosp("Portátil Ideapd 320 ", 444, 2);
+call datosp("Impresora HP Deskjet 3720", 59.99, 3);
+call datosp("Impresora HP Laserjet Pro M26nw", 180, 3);
+
 select * from fabricante;
+select * from producto;
--- Procedimientos---Celulares.sql
+++ Procedimientos---Celulares.sql
@@ -1,36 +1,37 @@
 /*---1---*/
 
 USE `celulares`;
-DROP procedure IF EXISTS `datosp`;
-
-DELIMITER $$
-USE `celulares`$$
-CREATE PROCEDURE datosp (
-	in id int,
-    in nombre varchar(100),
-    in valor double,
-    in fabricante int
-)
-BEGIN
-	insert into productos (codigo,nombre,precio,codigo_fabricante)
-    value (id,nombre,valor,fabricante);
-END$$
-
-DELIMITER ;
-/*---2---*/
-
-USE `celulares`;
 DROP procedure IF EXISTS `datosf`;
 
 DELIMITER $$
 USE `celulares`$$
 CREATE PROCEDURE datosf (
-	in id int,
     in nombre varchar(100)
 )
 BEGIN
-	insert into fabricante(codigo,nombre)
-    value (id,nombre);
+	insert into fabricante(nombre)
+    value (nombre);
 END$$
 
 DELIMITER ;
+
+
+/*---2---*/
+
+USE `celulares`;
+DROP procedure IF EXISTS `datosp`;
+
+DELIMITER $$
+USE `celulares`$$
+CREATE PROCEDURE datosp (
+    in nombre varchar(100),
+    in valor double,
+    in fabricante int
+)
+BEGIN
+	insert into producto (nombre,precio,codigo_fabricante)
+    value (nombre,valor,fabricante);
+END$$
+
+DELIMITER ;
+
